743586480178d865ef8e2941ec0c5c7f321a8118
Hantong Liu <hantongliu@outlook.com>
2023-02-19 16:11:55 -0800

    replace AsyncTask with ExecutorService in concurrent

--- app/src/main/java/edu/northeastern/numad23sp_group39project/APIService.java
+++ app/src/main/java/edu/northeastern/numad23sp_group39project/APIService.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -26,6 +28,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class APIService extends AppCompatActivity {
     private static final String TAG = "APIService";
@@ -73,70 +77,67 @@ public class APIService extends AppCompatActivity {
                 totalTask = movieList.size();
                 // launch http call here and parse result
                 for (String title : movieList) {
-                    PingWebServiceTask task = new PingWebServiceTask();
-                    task.execute(title);
+                    ExecutorService executor = Executors.newSingleThreadExecutor();
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    executor.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            String[] result = runHTTPCall(title);
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    synchronized (lock){
+                                        //store fetched result
+                                        searchRes.add(new String[]{result[0],result[1]});
+                                        lock.notifyAll();
+                                    }
+                                }
+                            });
+                        }
+                    });
+
                 }
                 new Thread(new ResultCollector()).start();
             }
         });
-
     }
 
-    private class PingWebServiceTask extends AsyncTask<String, Integer, String[]> {
+    public String[] runHTTPCall(String title) {
+        String[] results = new String[2];
+        URL url = null;
+        try {
+            url = new URL(getResources().getString(R.string.imdb_api) + title);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
 
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            Log.i(TAG, "Making progress...");
+            conn.connect();
+
+            InputStream inputStream = conn.getInputStream();
+            final String resp = convertStreamToString(inputStream);
+
+            // return [Title, imdbRating]
+            JSONObject jObject = new JSONObject(resp);
+            String jTitle = jObject.getString("Title");
+            String jRating = jObject.getString("imdbRating");
+            results[0] = jTitle;
+            results[1] = jRating;
+            return results;
+        } catch (MalformedURLException e) {
+            Log.e(TAG,"MalformedURLException");
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            Log.e(TAG,"ProtocolException");
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e(TAG,"IOException");
+            e.printStackTrace();
+        } catch (JSONException e) {
+            Log.e(TAG,"JSONException");
+            e.printStackTrace();
         }
-
-        @Override
-        protected String[] doInBackground(String... params) {
-            String[] results = new String[2];
-            URL url = null;
-            try {
-                url = new URL(getResources().getString(R.string.imdb_api) + params[0]);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setDoInput(true);
-
-                conn.connect();
-
-                InputStream inputStream = conn.getInputStream();
-                final String resp = convertStreamToString(inputStream);
-
-                // return [Title, imdbRating]
-                JSONObject jObject = new JSONObject(resp);
-                String jTitle = jObject.getString("Title");
-                String jRating = jObject.getString("imdbRating");
-                results[0] = jTitle;
-                results[1] = jRating;
-                return results;
-            } catch (MalformedURLException e) {
-                Log.e(TAG,"MalformedURLException");
-                e.printStackTrace();
-            } catch (ProtocolException e) {
-                Log.e(TAG,"ProtocolException");
-                e.printStackTrace();
-            } catch (IOException e) {
-                Log.e(TAG,"IOException");
-                e.printStackTrace();
-            } catch (JSONException e) {
-                Log.e(TAG,"JSONException");
-                e.printStackTrace();
-            }
-            results[0] = "Movie Not Found";
-            return(results);
-        }
-
-        @Override
-        protected void onPostExecute(String... s) {
-            super.onPostExecute(s);
-            //store fetched result
-            synchronized (lock){
-                searchRes.add(new String[]{s[0],s[1]});
-                lock.notifyAll();
-            }
-        }
+        results[0] = "Movie Not Found";
+        return(results);
     }
 
 //    collect results from all PingWebServiceTask and present them in a new activity
