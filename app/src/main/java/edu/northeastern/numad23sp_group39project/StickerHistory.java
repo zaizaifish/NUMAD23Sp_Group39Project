47bbe0576980c9dac7269a65b04d10f697af43ae
stevesun <sunguoqin@oneinfitech.com>
2023-03-05 13:51:52 -0800

    bring history button to a new page

---
+++ app/src/main/java/edu/northeastern/numad23sp_group39project/StickerHistory.java
@@ -0,0 +1,16 @@
+package edu.northeastern.numad23sp_group39project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StickerHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sticker_history);
+
+
+    }
+}
