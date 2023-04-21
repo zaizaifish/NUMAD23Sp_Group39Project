05c24b5f76ffe1f62d20cca760af10707e9c6642
WANG Tianchang Alex <wangtcalex@gmail.com>
2023-04-20 23:25:59 -0700

    feat: add background map view

--- app/src/main/java/edu/northeastern/numad23sp_group39project/FinalWorkoutDetailActivity.java
+++ app/src/main/java/edu/northeastern/numad23sp_group39project/FinalWorkoutDetailActivity.java
@@ -3,8 +3,14 @@ package edu.northeastern.numad23sp_group39project;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class FinalWorkoutDetailActivity extends AppCompatActivity {
+public class FinalWorkoutDetailActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     private TextView workoutType, workoutDate, workoutLength, workoutDistance, heartRate, caloriesBurned;
 
@@ -12,6 +18,9 @@ public class FinalWorkoutDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_workout_detail);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         workoutType = findViewById(R.id.workout_type);
         workoutDate = findViewById(R.id.workout_date);
@@ -22,5 +31,11 @@ public class FinalWorkoutDetailActivity extends AppCompatActivity {
 
         // Set values for text views from workout data (e.g., from an intent or a database)
 
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sanJose = new LatLng(37.3382, -121.8863);
+        googleMap.addMarker(new MarkerOptions().position(sanJose).title("Marker in San Jose"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sanJose, 10.0f));
     }
 }
