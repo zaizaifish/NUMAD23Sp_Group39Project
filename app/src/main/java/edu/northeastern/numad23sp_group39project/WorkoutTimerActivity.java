00bc6d55d4551982d8ad70c7e64abd76af3b1bc2
stevesun <sunguoqin@oneinfitech.com>
2023-04-21 14:04:52 -0700

    Add timer activity to each workout

---
+++ app/src/main/java/edu/northeastern/numad23sp_group39project/WorkoutTimerActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad23sp_group39project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorkoutTimerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout_timer);
+    }
+}
